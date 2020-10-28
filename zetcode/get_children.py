554c085db565eeae80629e874f9f6dbb2b443b06
Phil Lembo <phil.lembo@gmail.com>
2020-10-28 13:08:41 -0400

    Print children of html tag

---
+++ zetcode/get_children.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+
+with open('index.html', 'r') as f:
+
+    contents = f.read()
+
+    soup = BeautifulSoup(contents, 'lxml')
+
+    root = soup.html
+
+    root_childs = [e.name for e in root.children if e.name is not None]
+    print(root_childs)
