d91eddf510578cf5193184455075d7b31519c509
Phil Lembo <phil.lembo@gmail.com>
2020-10-28 12:59:43 -0400

    Simple script to retrieve page elements.

---
+++ zetcode/simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+
+with open('index.html', 'r') as f:
+
+    contents = f.read()
+
+    soup = BeautifulSoup(contents, 'lxml')
+
+    print(soup.h2)
+    print(soup.head)
+    print(soup.li)
